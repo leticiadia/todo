e78a762710d44a467526ee8b36bb52520154fd52
Leticia Dias <contatoleticiadia@gmail.com>
2024-08-11 11:31:47 -0300

    feat: section to return created task list

--- src/components/task/Task.tsx
+++ src/components/task/Task.tsx
@@ -1,13 +1,40 @@
 import { PlusCircle } from "@phosphor-icons/react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import styles from "./Task.module.css";
 
+interface TasksProps {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 export function Task(): any {
+  const [tasks, setTasks] = useState(["Nova Task"]);
+  const [newTasksText, setNewTasksText] = useState("");
+
+  function handleCreateNewTask(event: FormEvent) {
+    event.preventDefault();
+    setTasks((currentTask) => {
+      return [
+        ...currentTask,
+        { id: crypto.randomUUID(), content: newTasksText, completed: false },
+      ];
+    });
+  }
+
+  console.log(tasks);
+
+  function handlNewTaskChange(event: ChangeEvent<HTMLInputElement>) {
+    setNewTasksText(event.target.value);
+  }
+
   return (
     <div>
-      <form className={styles.taskForm}>
+      <form onSubmit={handleCreateNewTask} className={styles.taskForm}>
         <input
           type="text"
-          name="tasks"
+          value={newTasksText}
+          onChange={handlNewTaskChange}
           placeholder="Adicione uma nova tarefa"
         />
 
@@ -18,6 +45,20 @@ export function Task(): any {
           </button>
         </footer>
       </form>
+
+      <div className={styles.taskList}>
+        <div className={styles.headerTaskList}>
+          <div className={styles.createdTask}>
+            <p>Tarefas criadas</p>
+            <span>0</span>
+          </div>
+
+          <div className={styles.doneTask}>
+            <p>Concluídas</p>
+            <span>0</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
