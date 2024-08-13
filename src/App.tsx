57088a67bce6f530f5275403b8a4397c6143f173
Leticia Dias <contatoleticiadia@gmail.com>
2024-08-13 10:39:15 -0300

    feat: added Task component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-import "./App.css";
 import { Header } from "./components/header/Header";
+import { Task } from "./components/task/Task";
+
+import styles from "./App.module.css";
 
 function App() {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <main>
+          <Task />
+        </main>
+      </div>
+    </div>
+  );
 }
 
 export default App;
