f9299474c14493e45455e5bb813bc855353a6858
Leticia Dias <contatoleticiadia@gmail.com>
2024-09-07 11:07:07 -0300

    refactor: files renamed to NewTaskForm.

---
+++ src/components/task/NewTaskForm.tsx
@@ -0,0 +1,42 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+import { PlusCircle } from "@phosphor-icons/react";
+
+import styles from "./NewTaskForm.module.css";
+
+interface NewTodoFormProps {
+  onAddTask: (newTaskText: string) => void;
+}
+
+export function NewTakForm({ onAddTask }: NewTodoFormProps) {
+  const [newTaskText, setNewTaskText] = useState("");
+
+  function handleCreateNewTask(event: FormEvent) {
+    event.preventDefault();
+
+    onAddTask(newTaskText);
+
+    setNewTaskText("");
+  }
+
+  function handleNewTaskChange(event: ChangeEvent<HTMLInputElement>) {
+    setNewTaskText(event.target.value);
+  }
+
+  return (
+    <form onSubmit={handleCreateNewTask} className={styles.taskForm}>
+      <input
+        type="text"
+        value={newTaskText}
+        onChange={handleNewTaskChange}
+        placeholder="Adicione uma nova tarefa"
+      />
+
+      <footer>
+        <button type="submit">
+          Criar
+          <PlusCircle size={18} />
+        </button>
+      </footer>
+    </form>
+  );
+}
